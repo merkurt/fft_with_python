75938723364ec53371976c02d316a7e663593c84
merkurt <omerkurt099@gmail.com>
2020-11-11 10:13:35 +0300

    some bug fixes

--- signal_generator.py
+++ signal_generator.py
@@ -8,7 +8,7 @@ def signalGenerator(sampleRate, cosHz, sinHz):
     timeVector = np.arange(0, 1 - (1/Fs), (1/Fs))   # time vector   
     signal1 = np.cos(2 * np.pi * cosHz * timeVector)  # 100 Hz cos signal
     signal2 = np.sin(2 * np.pi * sinHz * timeVector)  # 150 Hz sin signal
-    noise = np.random.rand(timeVector.shape[0])  # some noise
+    noise = np.random.randn(timeVector.shape[0])  # some noise
     finalSignalWoNoise = signal1 + signal2  # final signal without noise
     finalSignal = finalSignalWoNoise + noise    # final signal with noise
     return timeVector, finalSignalWoNoise, finalSignal
